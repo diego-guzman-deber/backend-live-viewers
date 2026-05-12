01bcbb68410beb3e558673d1cea510e4f546558b
Diego Guzman <diegoguzman366@gmail.com>
2026-05-12 11:21:05 -0400

    first commit

--- src/main.ts
+++ src/main.ts
@@ -16,16 +16,24 @@ async function bootstrap() {
       'http://localhost:5174',
       'http://127.0.0.1:3001',
       'http://127.0.0.1:5173',
+
+      // acceso desde red interna
+      'http://172.20.16.38:5173',
     ],
     credentials: true,
   });
 
-  // Servir archivos estáticos de la carpeta public
+  // Servir archivos estáticos
   app.useStaticAssets(join(__dirname, '..', 'public'));
 
-  await app.listen(process.env.PORT ?? 3000, () => {
-    console.log(`🚀 Servidor corriendo en http://localhost:${process.env.PORT ?? 3000}`);
-    console.log(`📊 Ver tiempo real en http://localhost:${process.env.PORT ?? 3000}/realtime-viewer.html`);
-  });
+  const port = process.env.PORT ?? 3000;
+
+  // Escuchar en todas las interfaces (clave para servidor)
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`🚀 Servidor corriendo en puerto ${port}`);
+  console.log(`🌐 API: http://172.20.16.38:${port}`);
+  console.log(`📊 Viewer: http://172.20.16.38:${port}/realtime-viewer.html`);
 }
+
 bootstrap();
